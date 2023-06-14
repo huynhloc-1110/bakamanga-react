edd98d67c283de1c334c6a71f07abc71f97aadd6
huy26062002 <nonemu211@gmail.com>
2023-06-14 14:27:32 +0700

    Setup gif at banner and sfx, Add button for Most View and Rate

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,18 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Button, Col, Container, Row, Image } from 'react-bootstrap';
 import { getMangas } from '../../service/Data.service';
 import MangasList from '../../components/mangaList';
-import banner from '../../img/banner/banner.png'
+import banner from '../../img/banner/banner.gif';
 import "./styles.css";
 import CarouselFade from '../../components/carousel';
+import soundEffect from '../../img/sfx/sound-effect.mp3';
 
 function Home() {
+
+    const audioRef = useRef(null);
 
     const [activeButton, setActiveButton] = useState('LatestManga');
 
     const handleButtonClick = (sortOption) => {
         setActiveButton(sortOption);
     };
+
+    const handleSoundClick = () => {
+        if (audioRef.current) {
+            audioRef.current.currentTime = 0;
+            audioRef.current.play();
+        }
+      };
 
 
     const [mangas, setMangas] = useState([]);
@@ -38,19 +48,18 @@ function Home() {
                         <Col xs={12} md={6} xl={9}>
                             <div style={{ paddingTop: "30px" }}>
                                 <span className="tagline">
-                                    Welcome to the captivating world of manga!</span>
+                                    Welcome to 3kManga!</span>
                                 <p className='tagline-p '>
                                     Immerse yourself in an extraordinary assortment of manga.
                                     Action, adventure, fantasy, mystery, romance, and more—thousands of manga volumes for every fan!!</p>
                             </div>
                         </Col>
                         <Col xs={12} md={6} xl={3}>
-
                             <div >
-                                <Image style={{ width: "60%", height: "50%" }} src={banner} />
+                                <Image style={{ width: "60%", height: "50%" }} src={banner} onClick={handleSoundClick} />
+                                <audio ref={audioRef} src={soundEffect} /> 
                             </div>
-
-                        </Col>
+                        </Col> 
                     </Row>
                 </Container>
             </div>
@@ -72,10 +81,28 @@ function Home() {
                     >
                         Latest Chapter
                     </Button>
+                    &nbsp;
+                    <Button
+                        className='mb-4 w-100'
+                        variant={activeButton === 'MostView' ? 'dark' : 'light'}
+                        onClick={() => handleButtonClick('MostView')}
+                    >
+                        Most View Manga
+                    </Button>
+                    &nbsp;
+                    <Button
+                        className='mb-4 w-100'
+                        variant={activeButton === 'MostRate' ? 'dark' : 'light'}
+                        onClick={() => handleButtonClick('MostRate')}
+                    >
+                        Most Rate Manga
+                    </Button>
                 </div>
                 <div>
                     {activeButton === 'LatestManga' && <MangasList header="Latest Updated Manga" link="/Manga?sortOption=LatestManga" data={mangas} />}
                     {activeButton === 'LatestChapter' && <MangasList header="Latest Updated Chapter" link="/Manga?sortOption=LatestChapter" data={mangas} />}
+                    {/* {activeButton === 'MostView' && <MangasList header="Most View Manga" link="/Manga?sortOption=MostView" data={mangas} />}
+                    {activeButton === 'MostRate' && <MangasList header="Most Rate Manga" link="/Manga?sortOption=MostRate" data={mangas} />} */}
                 </div>
             </div>
         </div>
