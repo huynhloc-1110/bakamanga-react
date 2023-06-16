a953a13a1f1eae6e2b5dd0d6f13b83084ee78817
Trí <39791177+papazos@users.noreply.github.com>
2023-06-16 13:00:08 +0700

    Set up several dark mode components

---
+++ src/components/toggleTheme/themeContext.jsx
@@ -0,0 +1,22 @@
+// ThemeContext.js
+import React, { createContext, useState } from 'react';
+
+const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
+  const theme = isDarkMode ? 'dark' : 'light';
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export { ThemeProvider, ThemeContext };
