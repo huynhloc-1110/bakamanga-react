f0519400debc0ed4033ddf0dac755059611f0956
papazos <erizospvp@gmail.com>
2023-06-19 10:40:43 +0700

    Set up local storage of theme does not change after reloading the page

--- src/components/toggleTheme/themeContext.jsx
+++ src/components/toggleTheme/themeContext.jsx
@@ -1,13 +1,21 @@
-// ThemeContext.js
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme');
+    setIsDarkMode(storedTheme === 'dark');
+  }, []);
+
   const toggleTheme = () => {
-    setIsDarkMode((prevMode) => !prevMode);
+    setIsDarkMode((prevMode) => {
+      const newMode = !prevMode;
+      localStorage.setItem('theme', newMode ? 'dark' : 'light');
+      return newMode;
+    });
   };
 
   const theme = isDarkMode ? 'dark' : 'light';
